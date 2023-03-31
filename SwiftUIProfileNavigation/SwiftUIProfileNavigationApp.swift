4bc84090e66bab1594f4798ae55a75bb642cc7cf
smbrmoyo <brianmoyou@gmail.com>
2023-03-31 06:25:46 +0200

    Initial Commit

---
+++ SwiftUIProfileNavigation/SwiftUIProfileNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProfileNavigationApp.swift
+//  SwiftUIProfileNavigation
+//
+//  Created by Brian Moyou on 31.03.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProfileNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
